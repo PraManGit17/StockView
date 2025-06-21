611e807058ac0c52d38a942bb54b28d9ec8f25b3
Your NamePratham <pra172005@gmail.com>
2025-06-22 03:41:42 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,26 @@ const cors = require('cors');
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+// const allowedOrigins = ['http://localhost:5173', 'https://your-react-app.vercel.app'];
+
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true
+// }));
+
+
+
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
+
 app.use(express.json());
 const PORT = process.env.PORT;
 
